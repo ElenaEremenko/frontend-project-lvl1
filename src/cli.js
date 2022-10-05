4cc042eedb7ce27c1ef9cf7fb109b241269cc719
Elena Eremenko <elena.eremenko452@gmail.com>
2022-10-05 13:29:43 +0600

    Fix code in brain-games

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   const phrase = `Hello, ${userName}!`;
   console.log(phrase);
