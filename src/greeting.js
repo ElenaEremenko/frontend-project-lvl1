53814f8b6259f6b214190bcff7dcf589c89a0fa0
Elena Eremenko <elena.eremenko452@gmail.com>
2022-10-05 13:38:06 +0600

    Remove greeting.js

--- src/greeting.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-
-export const userName = readlineSync.question('May I have your name? ');
-export const greeting = () => {
-  const phrase = `Hello, ${userName}!`;
-  console.log(phrase);
-};
